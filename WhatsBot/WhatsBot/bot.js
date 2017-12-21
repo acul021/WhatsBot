77bb602ca0427ffc4e101456595d4d11196112ed
BuildTools <unconfigured@null.spigotmc.org>
2017-12-21 21:38:45 +0100

    Test

--- WhatsBot/WhatsBot/bot.js
+++ WhatsBot/WhatsBot/bot.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         What's Bot
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://web.whatsapp.com/
