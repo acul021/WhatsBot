9d29683f8f17ce20014f043c8242c0896d06dccf
BuildTools <unconfigured@null.spigotmc.org>
2017-12-21 21:32:09 +0100

    test

--- WhatsBot/WhatsBot/bot.js
+++ WhatsBot/WhatsBot/bot.js
@@ -1,1 +1,16 @@
-﻿
+﻿// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        https://web.whatsapp.com/
+// @require      http://code.jquery.com/jquery-latest.min.js
+// @downloadURL  https://raw.githubusercontent.com/acul021/WhatsBot/master/WhatsBot/WhatsBot/bot.js
+// @updateURL    https://raw.githubusercontent.com/acul021/WhatsBot/master/WhatsBot/WhatsBot/bot.js
+// @grant        none
+// ==/UserScript==
+
+$(document).ready(function () {
+
+});
